ce074f44b701c9b95fb37c7a3828510fced830ac
Jakub Olszewski <olszewskikuba17@gmail.com>
2023-10-28 16:59:27 +0200

    Frontend init

---
+++ frontend/components/siteHeader.vue
@@ -0,0 +1,203 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" fixed app temporary>
+      <v-list dense>
+        <v-list-item-group v-for="(item, i) in items" :key="i" color="primary">
+          <v-list-item v-if="!item.submenu" :to="item.to">
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title.toUpperCase()" />
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-group v-else :prepend-icon="item.icon" no-action>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title
+                  v-text="item.title.toUpperCase()"
+                ></v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item
+              v-for="child in item.submenu"
+              :key="child.title"
+              :to="child.to"
+            >
+              <v-list-item-content>
+                <v-list-item-title v-text="child.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar fixed app hide-on-scroll height="64" elevate-on-scroll>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = true" />
+      <nuxt-link to="/" class="d-flex">
+        <Logo />
+      </nuxt-link>
+      <v-spacer />
+
+      <template v-for="(name, menuitem) in items">
+        <template v-if="name.submenu">
+          <v-menu
+            :key="menuitem"
+            open-on-hover
+            offset-y
+            transition="slide-y-transition"
+            bottom
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                plain
+                class="py-5 submenubtn hidden-sm-and-down"
+                :ripple="false"
+                v-bind="attrs"
+                style="height: auto"
+                v-on="on"
+              >
+                {{ name.title }}
+                <v-icon right small class="mx-0"> mdi-chevron-down </v-icon>
+              </v-btn>
+            </template>
+            <v-list dense>
+              <v-list-item
+                v-for="(item, index) in name.submenu"
+                :key="index"
+                link
+                :to="item.to"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+        <v-btn
+          v-else
+          :key="menuitem"
+          depressed
+          tile
+          plain
+          class="py-8 hidden-sm-and-down"
+          :to="name.to"
+          >{{ name.title }}</v-btn
+        > </template
+      ><v-spacer />
+
+      <v-btn icon @click="changeThemeColor">
+        <v-icon>{{
+          $vuetify.theme.dark ? 'mdi-white-balance-sunny' : 'mdi-weather-night'
+        }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-folder-home-outline',
+          title: 'Strona domowa',
+          to: '/',
+        },
+        {
+          icon: 'mdi-account',
+          title: 'O nas',
+          to: '/about',
+        },
+        {
+          icon: 'mdi-tools',
+          title: 'Wydarzenia',
+          submenu: [
+            {
+              title: 'Zapisz się',
+              to: '/events/join',
+            },
+            {
+              title: 'Odkrywaj',
+              to: '/events/explore',
+            },
+            {
+              title: 'Moje wydarzenia',
+              to: '/events/my',
+            },
+          ],
+        },
+        {
+          icon: 'mdi-tools',
+          title: 'Spotkania',
+          submenu: [
+            {
+              title: 'Zapisz się',
+              to: '/meetings/join',
+            },
+            {
+              title: 'Odkrywaj',
+              to: '/meetings/explore',
+            },
+            {
+              title: 'Moje spotkania',
+              to: '/meetings/my',
+            },
+          ],
+        },
+        {
+          icon: 'mdi-cash-usd',
+          title: 'Cennik',
+          to: '/pricing',
+        },
+        {
+          icon: 'mdi-blogger',
+          title: 'Mój profil',
+          to: '/profile',
+          submenu: this.userProfileSubpages(),
+        },
+      ],
+      cookie: { token: 'asd' },
+    }
+  },
+  methods: {
+    changeThemeColor() {
+      if (this.$vuetify.theme.dark === true) {
+        this.$vuetify.theme.dark = false
+      } else {
+        this.$vuetify.theme.dark = true
+      }
+    },
+    userProfileSubpages() {
+      if (this.cookie?.token) {
+        return [
+          {
+            title: 'Zarządzaj profilem',
+            to: '/user/manage',
+          },
+          {
+            title: 'Wyloguj',
+            to: '/user/logout',
+          },
+        ]
+      } else {
+        return [
+          {
+            title: 'Zarejestruj',
+            to: '/user/register',
+          },
+        ]
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.submenubtn {
+  cursor: default;
+}
+</style>
---
+++ frontend/pages/user/register.vue
@@ -0,0 +1,109 @@
+<template>
+  <section>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <SectionsHeroAlt :hero-alt="heroAlt" />
+        <v-container>
+          <h2
+            class="text-h4 text-md-h3 text-center font-weight-black text-capitalize"
+          >
+            REJESTRACJA
+          </h2>
+        </v-container>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      heroAlt: [
+        {
+          src: 'pexels-moose-photos-1036641.jpg',
+          heading: ' About Us ',
+        },
+      ],
+      ourTeam: [
+        {
+          name: 'John Churchill',
+          position: 'Marketing Director',
+          phone: '+1 (987) 1625346',
+          email: 'john@example.com',
+          photo: 'person-1.jpg',
+        },
+        {
+          name: 'Fiona	Ross',
+          position: 'Project Manager',
+          phone: '+1 (987) 5894684',
+          email: 'fiona@example.com',
+          photo: 'person-2.jpg',
+        },
+        {
+          name: 'Justin	Rees',
+          position: 'VP Marketing',
+          phone: '+1 (987) 6982456',
+          email: 'justin@example.com',
+          photo: 'person-3.jpg',
+        },
+        {
+          name: 'Amelia	Ogden',
+          position: 'Communication Manager',
+          phone: '+1 (987) 6982456',
+          email: 'amelia@example.com',
+          photo: 'person-4.jpg',
+        },
+        {
+          name: 'Sebastian Bailey',
+          position: 'Advertising Director',
+          phone: '+1 (987) 6982456',
+          email: 'sebastian@example.com',
+          photo: 'person-5.jpg',
+        },
+        {
+          name: 'Eric Sutton',
+          position: 'Advertising Executive',
+          phone: '+1 (987) 6982456',
+          email: 'audrey@example.com',
+          photo: 'person-6.jpg',
+        },
+        {
+          name: 'Xia Yen',
+          position: 'Advertising Manager',
+          phone: '+1 (987) 6982456',
+          email: 'christian@example.com',
+          photo: 'person-7.jpg',
+        },
+        {
+          name: 'Bernadette	Springer',
+          position: 'Employee Relations Manager',
+          phone: '+1 (987) 6982456',
+          email: 'bernadette@example.com',
+          photo: 'person-8.jpg',
+        },
+        {
+          name: 'Elizabeth Newman',
+          position: 'Project Manager',
+          phone: '+1 (987) 6982456',
+          email: 'elizabeth@example.com',
+          photo: 'person-9.jpg',
+        },
+      ],
+    }
+  },
+  head() {
+    return {
+      title: 'About Us',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Infographic hypotheses influencer user experience Long madel ture gen-z paradigm shift client partner network product seilans solve management influencer analytics leverage virality. incubator seed round massmarket. buyer agile development growth hacking business-to-consumer ecosystem',
+        },
+      ],
+    }
+  },
+}
+</script>
